466ae845cbcb4250f0b85a45b68c70f552831ce5
edomello <edomello@gmail.com>
2014-01-25 14:53:58 +0100

    Livello1

--- Assets/Script/MuoviPersonaggio.cs
+++ Assets/Script/MuoviPersonaggio.cs
@@ -22,21 +22,40 @@ public class MuoviPersonaggio : MonoBehaviour {
 			float speedIncrese = 500;
 			float rotateSpeed = 50f;
 
-			float rotazione = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+			float rotazioneX = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
 			bool retro = false;
-			if (rotazione < 0){
+			if (rotazioneX < 0){
 				retro = true;
 			}
-			rotazione = Mathf.Min(gestoreEnergia.getEnergia(), Mathf.Abs(rotazione) );
-			rotazione = Mathf.Round(rotazione*100f)/100f;
-			gestoreEnergia.subEnergia(rotazione/rotateSpeed);
+			rotazioneX = Mathf.Min(gestoreEnergia.getEnergia(), Mathf.Abs(rotazioneX) );
+			rotazioneX = Mathf.Round(rotazioneX*100f)/100f;
+			gestoreEnergia.subEnergia(rotazioneX/rotateSpeed);
 			if (retro){
-				rotazione *= -1;
+				rotazioneX *= -1;
 			}
 			//transform.Rotate(Vector3.up, rotazione);
 			//Debug.Log("rotazione:"+rotazione);
-			c.AddRelativeTorque(Vector3.up * rotazione);
-		
+			Quaternion deltaRotation = Quaternion.Euler(Vector3.up * rotazioneX);
+			c.MoveRotation(c.rotation * deltaRotation);
+			//c.MoveRotation();
+			
+			float rotazioneY = Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+			retro = false;
+			if (rotazioneY < 0){
+				retro = true;
+			}
+			rotazioneY = Mathf.Min(gestoreEnergia.getEnergia(), Mathf.Abs(rotazioneY) );
+			rotazioneY = Mathf.Round(rotazioneY*100f)/100f;
+			gestoreEnergia.subEnergia(rotazioneY/rotateSpeed);
+			if (retro){
+				rotazioneY *= -1;
+			}
+			//transform.Rotate(Vector3.up, rotazione);
+			//Debug.Log("rotazione:"+rotazione);
+			deltaRotation = Quaternion.Euler(Vector3.up * rotazioneY);
+			c.MoveRotation(c.rotation * deltaRotation);
+			//c.MoveRotation();
+
 			float velocita = Input.GetAxis("Vertical") * speedIncrese * Time.deltaTime;
 			retro = false;
 			if (velocita < 0){
