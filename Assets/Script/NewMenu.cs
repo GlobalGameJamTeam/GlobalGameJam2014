b21e66b853157b7e92e0ebd32a0525982362c162
edomello <edomello@gmail.com>
2014-01-27 22:27:22 +0100

    LastVersion
    Post-GGJ

---
+++ Assets/Script/NewMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewMenu : MonoBehaviour {
+
+	private float up = (Screen.height - 345) / 2;
+	private float left = (Screen.width  - 50) / 2;
+
+	/*
+	void OnStart (){
+		public float left = (Screen.height ) / 2;
+		public float up = (Screen.width ) / 2;
+	}*/
+	
+	void OnGUI () {
+		if(GUI.Button (new Rect (left,up,115,50), "Livello 1")) {
+			Application.LoadLevel ("Livello1");
+		}
+
+		if(GUI.Button (new Rect (left,up + 60,115,50), "Livello 2")) {
+			Application.LoadLevel ("livello_prova");
+		}
+
+		if(GUI.Button (new Rect (left,up + 120,115,50), "Quit")) {
+			Application.Quit();
+		}
+	}
+}
---
+++ Assets/Script/onclickToMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onclickToMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//onclick
+		string scene = Application.loadedLevelName;
+		Application.LoadLevel(scene);
+	}
+}
